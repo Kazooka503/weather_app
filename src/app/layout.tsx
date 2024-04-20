d614d614a650ae2d298c6268c572e9f77597f5e7
Austin <austinkrz@gmail.com>
2024-04-20 00:13:05 -0700

    Data Fetching Styling Progress Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <QueryClientProvider client={queryClient}>
-      <body className={inter.className}>{children}</body>
+        <body className={inter.className}>{children}</body>
       </QueryClientProvider>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,63 +5,67 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import { useQuery } from "react-query";
 import axios from "axios";
+import { format, parseISO } from "date-fns";
+import Container from "./components/Container";
+import { convertKelvinToCelsius } from "./utils/convertKelvinToCelsius";
 
 //https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=3002e77e3876cdadf1f04dadb8c76962
-type WeatherData = {
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+interface WeatherData {
   cod: string;
   message: number;
   cnt: number;
-  list: {
-      dt: number;
-      main: {
-          temp: number;
-          feels_like: number;
-          temp_min: number;
-          temp_max: number;
-          pressure: number;
-          sea_level: number;
-          grnd_level: number;
-          humidity: number;
-          temp_kf: number;
-      };
-      weather: {
-          id: number;
-          main: string;
-          description: string;
-          icon: string;
-      }[];
-      clouds: {
-          all: number;
-      };
-      wind: {
-          speed: number;
-          deg: number;
-          gust: number;
-      };
-      visibility: number;
-      pop: number;
-      sys: {
-          pod: string;
-      };
-      dt_txt: string;
-  }[];
+  list: WeatherDetail[];
   city: {
-      id: number;
-      name: string;
-      coord: {
-          lat: number;
-          lon: number;
-      };
-      country: string;
-      population: number;
-      timezone: number;
-      sunrise: number;
-      sunset: number;
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
   };
-};
+}
 
 export default function Home() {
-
   const { isLoading, error, data } = useQuery<WeatherData>('repoData', async () =>
     {
       const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`);
@@ -69,11 +73,49 @@ export default function Home() {
     }
   );
 
+  const firstData = data?.list[0];
+
   console.log("data: ", data?.city.name)
+
+  if (isLoading) 
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    )
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar/>
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/* today's data */}
+        <section>
+          <div>
+            <span id="date" className="flex gap-1 text-2xl items-end">
+              {format(parseISO(firstData?.dt_txt ??''), 'EEEE')} <span className="text-lg">{format(parseISO(firstData?.dt_txt ??''), 'dd.MM.yyyy')}</span>
+            </span>
+            <Container className="gap-10 px-6 items-center">
+              <div className=" flex flex-col px-4 ">
+                <span className="text-5xl">{convertKelvinToCelsius(firstData?.main.temp ?? 0)}°</span>
+                <p className="text-xs space-x-1 whitespace-nowrap"></p>
+                <p className="text-xs space-x-2">
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp ?? 0)}
+                    °↓
+                  </span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp ?? 0)}
+                    °↑
+                  </span>
+                </p>
+              </div>
+            </Container>
+          </div>
+        </section>
+
+        {/* 7 day forcast data */}
+        <section></section>
+      </main>
     </div>
   );
 }
