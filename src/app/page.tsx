d5393af4928d192145c3e4e0b01530e4f07ca1a1
Austin <austinkrz@gmail.com>
2024-04-22 11:42:16 -0700

    Updates tsconfig.json to allow for specific data manipulation,
    Updates page.tsx with new data rendering
    
    Adds ForecastWeatherDetail - Component for displaying future weather data
    
    Adds WeatherDetails - Component for displaying weather data
    
    Adds convertWindSpeed - helper function for converting speed
    
    Adds metersToKilometers - helper function to convert meters to kilometers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { format, parseISO } from "date-fns";
+import { format, fromUnixTime, parseISO } from "date-fns";
 import Container from "./components/Container";
 import { convertKelvinToCelsius } from "./utils/convertKelvinToCelsius";
 import WeatherIcon from "./components/WeatherIcon";
 import { getDayOrNightIcon } from "./utils/getDayOrNightIcon";
+import { metersToKilometers } from "./utils/metersToKilometers";
+import WeatherDetails from "./components/WeatherDetails";
+import { convertWindSpeed } from "./utils/convertWindSpeed";
+import ForecastWeatherDetail from "./components/ForecastWeatherDetail";
 
 //https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=3002e77e3876cdadf1f04dadb8c76962
 interface WeatherDetail {
@@ -30,10 +34,10 @@ interface WeatherDetail {
     main: string;
     description: string;
     icon: string;
-  }[];
+  }[]
   clouds: {
     all: number;
-  };
+  }
   wind: {
     speed: number;
     deg: number;
@@ -64,7 +68,7 @@ interface WeatherData {
     timezone: number;
     sunrise: number;
     sunset: number;
-  };
+  }
 }
 
 export default function Home() {
@@ -73,11 +77,27 @@ export default function Home() {
       const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`);
       return data
     }
-  );
+  )
 
-  const firstData = data?.list[0];
+  const firstData = data?.list[0]
 
   console.log("data: ", data?.city.name)
+
+  const uniqueDates = [
+    ...new Set(
+      data?.list.map(
+        (entry) => new Date(entry.dt * 1000).toISOString().split("T")[0]
+      )
+    )
+  ]
+
+  const firstDataForEachDate = uniqueDates.map((date) => {
+    return data?.list.find((entry) => {
+      const entryDate = new Date(entry.dt * 1000).toISOString().split("T")[0]
+      const entryTime = new Date(entry.dt * 1000).getHours();
+      return entryDate === date && entryTime >= 6
+    })
+  })
 
   if (isLoading) 
     return (
@@ -131,10 +151,53 @@ export default function Home() {
               </div>
             </Container>
           </div>
+          <div className="flex gap-4">
+                {/* left */}
+                <Container className="w-fit justify-center flex-col px-4 items-center">
+                  <p className="capitalize text-center">{firstData?.weather[0].description}</p>
+                  <WeatherIcon iconName={getDayOrNightIcon(
+                    firstData?.weather[0].icon ?? "",
+                    firstData?.dt_txt ?? ""
+                  )}
+                  />
+                </Container>
+                {/* right */}
+                <Container className="bg-yellow-300/80 px-6 justify-between overflow-x-auto">
+                  <WeatherDetails visability={metersToKilometers(firstData?.visibility ?? 10000)} airPressure={`${firstData?.main.pressure} hPa`} 
+                    humdity={`${firstData?.main.humidity}%`} sunrise={format(fromUnixTime(data?.city.sunrise ?? 1702949452), "H:mm")}
+                    sunset={format(fromUnixTime(data?.city.sunset ?? 1702949452), "H:mm")} windSpeed={convertWindSpeed(firstData?.wind.speed ?? 1.64)}
+                  />
+                </Container>
+              </div>
         </section>
 
         {/* 7 day forcast data */}
-        <section></section>
+        <section className="flex w-full flex-col gap-4">
+          <p className="text-2xl">Forcast (7 days)</p>
+          {firstDataForEachDate.map((d, i) =>
+            <ForecastWeatherDetail 
+            key={i}
+            description={d?.weather[0].description ?? ""}
+            weatherIcon={d?.weather[0].icon ?? "01d"}
+            date={format(parseISO(d?.dt_txt ?? ""), "dd.MM")}
+            day={format(parseISO(d?.dt_txt ?? ""), "EEEE")}
+            feels_like={d?.main.feels_like ?? 0}
+            temp={d?.main.temp ?? 0}
+            temp_max={d?.main.temp_max ?? 0}
+            temp_min={d?.main.temp_min ?? 0}
+            airPressure={`${d?.main.pressure} hpa`}
+            humdity={`${d?.main.humidity}%`}
+            sunrise={format(
+              fromUnixTime(data?.city.sunrise ?? 1702517657), "H:mm"
+            )}
+            sunset={format(
+              fromUnixTime(data?.city.sunset ?? 1702517657), "H:mm"
+            )}
+            visability={`${metersToKilometers(d?.visibility ?? 100000)}`}
+            windSpeed={`${convertWindSpeed(d?.wind.speed ?? 1.64)}`}
+            />
+          )}
+        </section>
       </main>
     </div>
   );
