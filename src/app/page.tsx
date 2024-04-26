42e57bbb649e3f4c0e4a8dc2348a03058ccff387
Austin <austinkrz@gmail.com>
2024-04-25 20:02:46 -0700

    Adds ability to use search bar to look up specific city's weather data, adds city suggestions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,83 +1,92 @@
 'use client'
 
 
-import Image from "next/image";
-import Navbar from "./components/Navbar";
-import { useQuery } from "react-query";
-import axios from "axios";
-import { format, fromUnixTime, parseISO } from "date-fns";
-import Container from "./components/Container";
-import { convertKelvinToCelsius } from "./utils/convertKelvinToCelsius";
-import WeatherIcon from "./components/WeatherIcon";
-import { getDayOrNightIcon } from "./utils/getDayOrNightIcon";
-import { metersToKilometers } from "./utils/metersToKilometers";
-import WeatherDetails from "./components/WeatherDetails";
-import { convertWindSpeed } from "./utils/convertWindSpeed";
-import ForecastWeatherDetail from "./components/ForecastWeatherDetail";
+import Image from "next/image"
+import Navbar from "./components/Navbar"
+import { useQuery } from "react-query"
+import axios from "axios"
+import { format, fromUnixTime, parseISO } from "date-fns"
+import Container from "./components/Container"
+import { convertKelvinToCelsius } from "./utils/convertKelvinToCelsius"
+import WeatherIcon from "./components/WeatherIcon"
+import { getDayOrNightIcon } from "./utils/getDayOrNightIcon"
+import { metersToKilometers } from "./utils/metersToKilometers"
+import WeatherDetails from "./components/WeatherDetails"
+import { convertWindSpeed } from "./utils/convertWindSpeed"
+import ForecastWeatherDetail from "./components/ForecastWeatherDetail"
+import { loadingCityAtom, placeAtom } from "./atom"
+import { useAtom } from "jotai"
+import { useEffect } from "react"
 
 //https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=3002e77e3876cdadf1f04dadb8c76962
 interface WeatherDetail {
-  dt: number;
+  dt: number
   main: {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    sea_level: number;
-    grnd_level: number;
-    humidity: number;
-    temp_kf: number;
-  };
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    sea_level: number
+    grnd_level: number
+    humidity: number
+    temp_kf: number
+  }
   weather: {
-    id: number;
-    main: string;
-    description: string;
-    icon: string;
+    id: number
+    main: string
+    description: string
+    icon: string
   }[]
   clouds: {
-    all: number;
+    all: number
   }
   wind: {
-    speed: number;
-    deg: number;
-    gust: number;
-  };
-  visibility: number;
-  pop: number;
+    speed: number
+    deg: number
+    gust: number
+  }
+  visibility: number
+  pop: number
   sys: {
-    pod: string;
-  };
-  dt_txt: string;
+    pod: string
+  }
+  dt_txt: string
 }
 
 interface WeatherData {
-  cod: string;
-  message: number;
-  cnt: number;
-  list: WeatherDetail[];
+  cod: string
+  message: number
+  cnt: number
+  list: WeatherDetail[]
   city: {
-    id: number;
-    name: string;
+    id: number
+    name: string
     coord: {
-      lat: number;
-      lon: number;
-    };
-    country: string;
-    population: number;
-    timezone: number;
-    sunrise: number;
-    sunset: number;
+      lat: number
+      lon: number
+    }
+    country: string
+    population: number
+    timezone: number
+    sunrise: number
+    sunset: number
   }
 }
 
 export default function Home() {
-  const { isLoading, error, data } = useQuery<WeatherData>('repoData', async () =>
+  const [place, setPlace] = useAtom(placeAtom)
+  const [loadingCity, ] = useAtom(loadingCityAtom)
+  const { isLoading, error, data, refetch } = useQuery<WeatherData>('repoData', async () =>
     {
-      const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`);
+      const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`)
       return data
     }
   )
+
+  useEffect(() => {
+    refetch()
+  }, [place, refetch])
 
   const firstData = data?.list[0]
 
@@ -94,7 +103,7 @@ export default function Home() {
   const firstDataForEachDate = uniqueDates.map((date) => {
     return data?.list.find((entry) => {
       const entryDate = new Date(entry.dt * 1000).toISOString().split("T")[0]
-      const entryTime = new Date(entry.dt * 1000).getHours();
+      const entryTime = new Date(entry.dt * 1000).getHours()
       return entryDate === date && entryTime >= 6
     })
   })
@@ -108,9 +117,12 @@ export default function Home() {
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-      <Navbar/>
+      <Navbar location={data?.city.name}/>
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
         {/* today's data */}
+
+        {loadingCity ? <WeatherSkeleton/> :
+        <>
         <section className="space-y-4">
           <div className="space-y-2">
             <span id="date" className="flex gap-1 text-2xl items-end">
@@ -198,7 +210,48 @@ export default function Home() {
             />
           )}
         </section>
+        </>}
       </main>
     </div>
-  );
+  )
+}
+
+function WeatherSkeleton() {
+  return (
+    <section className="space-y-8 ">
+      {/* Today's data skeleton */}
+      <div className="space-y-2 animate-pulse">
+        {/* Date skeleton */}
+        <div className="flex gap-1 text-2xl items-end ">
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+        </div>
+
+        {/* Time wise temperature skeleton */}
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {[1, 2, 3, 4].map((index) => (
+            <div key={index} className="flex flex-col items-center space-y-2">
+              <div className="h-6 w-16 bg-gray-300 rounded"></div>
+              <div className="h-6 w-6 bg-gray-300 rounded-full"></div>
+              <div className="h-6 w-16 bg-gray-300 rounded"></div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* 7 days forecast skeleton */}
+      <div className="flex flex-col gap-4 animate-pulse">
+        <p className="text-2xl h-8 w-36 bg-gray-300 rounded"></p>
+
+        {[1, 2, 3, 4, 5, 6, 7].map((index) => (
+          <div key={index} className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+            <div className="h-10 w-10 bg-gray-300 rounded-full"></div>
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
 }
