006c0c6f3a81133d214580dc1179fce75b4d226b
Austin <austinkrz@gmail.com>
2024-04-25 20:25:30 -0700

    Adds ability to use geolocation for checking local user weather, adds responsiveness to nav bar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -67,36 +67,75 @@ export default function Navbar({location}: Props) {
 
   }
 
+  function handleCurrentLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(async(position)=>{
+            const { latitude, longitude } = position.coords
+            try{
+                setLoadingCity(true)
+                const response = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+                )
+                setTimeout(() => {
+                    setLoadingCity(false)
+                    setPlace(response.data.name)
+                }, 500)
+            } catch (error) {
+                setLoadingCity(false)
+            }
+        })
+    }
+  }
+
 
   return (
-    <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
-        <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
-            <div className='flex items-center justify-center gap-2'>
-                <h2 className='text-gray-500 text-3xl'>Weather</h2>
-                <MdWbSunny className='text-3xl mt-1 text-yellow-300'/>
+    <>
+        <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
+            <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
+                <div className='flex items-center justify-center gap-2'>
+                    <h2 className='text-gray-500 text-3xl'>Weather</h2>
+                    <MdWbSunny className='text-3xl mt-1 text-yellow-300'/>
+                </div>
+                {/* */}
+                <section className='flex gap-2 items-center'>
+                    <MdMyLocation onClick={handleCurrentLocation} title='Your Current Location' className='text-2xl text-gray-400 hover:opacity-80 cursor-pointer'/>
+                    <MdOutlineLocationOn className='text-3xl' />
+                    <p className='text-slate-900/80 text-sm'>{location}</p>
+                    <div className='relative hidden md:flex'>
+                        <SearchBox 
+                        value={city}
+                        onSubmit={handleSubmitSearch}
+                        onChange={(e) => handleInputChange(e.target.value)} />
+                        <SuggestionBox  
+                            {...{
+                                showSuggestions,
+                                suggestions,
+                                handleSuggestionClick,
+                                error
+                            }}
+                        />
+                    </div>
+                </section>
             </div>
-            {/* */}
-            <section className='flex gap-2 items-center'>
-                <MdMyLocation className='text-2xl text-gray-400 hover:opacity-80 cursor-pointer'/>
-                <MdOutlineLocationOn className='text-3xl' />
-                <p className='text-slate-900/80 text-sm'>{location}</p>
-                <div className='relative'>
-                    <SearchBox 
+        </nav>
+        <section className='flex max-w-7xl px-3 md:hidden'>
+            <div className='relative'>
+                <SearchBox 
                     value={city}
                     onSubmit={handleSubmitSearch}
-                    onChange={(e) => handleInputChange(e.target.value)} />
-                    <SuggestionBox  
-                        {...{
-                            showSuggestions,
-                            suggestions,
-                            handleSuggestionClick,
-                            error
-                        }}
-                    />
-                </div>
-            </section>
-        </div>
-    </nav>
+                    onChange={(e) => handleInputChange(e.target.value)} 
+                />
+                <SuggestionBox  
+                    {...{
+                        showSuggestions,
+                        suggestions,
+                        handleSuggestionClick,
+                        error
+                    }}
+                />
+            </div>
+        </section>
+    </>
   )
 }
 
